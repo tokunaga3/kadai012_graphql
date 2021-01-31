030375c712149d9a9bf1a19e3623f321d7927d4d
zakimiii <Th2786314@icloud.com>
2021-01-31 16:31:39 +0900

    mentor fix

--- app/graphql/types/blog_type.rb
+++ app/graphql/types/blog_type.rb
@@ -1,9 +1,6 @@
-module Types
-  class BlogType < Types::BaseObject
-    field :id, ID #, null: true
-    field :title, String #, null: true
-    field :text, String #, null: true
-    #field :created_at, GraphQL::Types::ISO8601DateTime, null: true
-    #field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
-  end
+Types::BlogType = GraphQL::ObjectType.define do
+  name "Blog"
+  field :id, !types.ID
+  field :title, !types.String
+  field :text, !types.String
 end
